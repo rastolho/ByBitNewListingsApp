ae9fc95b5943d1e8bb802f99f3e9b58251780e28
rastolho <pedro-vicentepc@hotmail.com>
2025-11-30 13:18:00 +0000

    - Change Bybit Service - Change app settings to get configs from hostContect.Configuration

--- ByBitNewListingsApp/Configuration/AppSettings.cs
+++ ByBitNewListingsApp/Configuration/AppSettings.cs
@@ -8,7 +8,7 @@ namespace ByBitNewListingsApp.Configuration
 {
     public class AppSettings
     {
-        public string? TelegramBotToken { get; set; }
+        public string? TelegramBotToken { get; set; } 
         public string? TelegramChatId { get; set; }
         public string Locale { get; set; } = "en-US";
         public int CheckIntervalSeconds { get; set; } = 300; // 5 minutes for GitHub Actions
--- ByBitNewListingsApp/Dtos/Listing.cs
+++ ByBitNewListingsApp/Dtos/Listing.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ByBitNewListingsApp.Dtos
 {
     public class Listing
     {
+        [JsonPropertyName("title")]
         public string? Title { get; set; }
+
+        [JsonPropertyName("description")]
         public string? Description { get; set; }
+        [JsonPropertyName("publishTime")]
         public long PublishTime { get; set; }
+        [JsonPropertyName("url")]
         public string? Url { get; set; }
+
+        [JsonPropertyName("tags")]
         public List<string>? Tags { get; set; }
     }
 }
